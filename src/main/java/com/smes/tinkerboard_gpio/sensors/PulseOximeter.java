f325712655c81c1e87e46638333350c382e10d2b
Lena Voytek <dvoytek@email.arizona.edu>
2020-05-14 22:21:23 -0700

    Removed address in const

--- src/main/java/com/smes/tinkerboard_gpio/sensors/PulseOximeter.java
+++ src/main/java/com/smes/tinkerboard_gpio/sensors/PulseOximeter.java
@@ -2,7 +2,7 @@ package com.smes.tinkerboard_gpio.sensors;
 
 public class PulseOximeter extends I2CSensor
 {
-	public PulseOximeter(String name, byte address, int scl, int sda)
+	public PulseOximeter(String name, int scl, int sda)
 	{
 		super(name, (byte)0xAE, scl, sda);
 	}
